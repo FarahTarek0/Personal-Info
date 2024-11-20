2ed93bc9a9314f145042fa8a75e1fcff7c4339eb
FarahTarek0 <farahtarek200002@gmail.com>
2024-11-20 18:25:34 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+name = str(input("Please enter your name: "))
+age = int(input("Please enter your age: "))
+height = float(input("Please enter your height in meters: "))
+is_student_input = input("Are you a student? (yes/no): ").lower()
+birth_year = (2024 - age)
+
+
+x = ((t + r) - (1 / 22))
+
+# all char lowercase, seprate by _ 
+is_student = True if is_student_input == 'yes' else False
+
+
+print("\n--- Personal Form ---")
+print(f"Name: {name}")
+print(f"Age: {age} years")
+print(f"Height: {height} meters")
+print(f"Is a student: {is_student}")
+print(f"Year of Birth :{birth_year}")
+
+
+class YearBook():
+    pass
+
+
+def year_book():
+    pass
+
+x = YearBook()
+
+
